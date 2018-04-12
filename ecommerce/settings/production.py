b2c84511a115bf6f3f8e0ea681621b29d8e9e17a
bill00lee@hotmail.com <bill00lee@hotmail.com>
2018-04-12 12:15:54 -0400

    debug true

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -23,7 +23,19 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'bill00lee@gmail.com'
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = 'Python ecommerce <bill00lee@gmail.com>'
+# BASE_URL = 'https://eelecommerce.herokuapp.com//'
+#
+# MANAGERS = (
+#     ('Will', "bill00lee@gmail.com"),
+# )
 
 MAILCHIMP_API_KEY = os.environ.get("MAILCHIMP_API_KEY")
 MAILCHIMP_DATA_CENTER = "us17"
@@ -35,7 +47,7 @@ STRIPE_PUB_KEY = os.environ.get("STRIPE_PUB_KEY", 'pk_test_PrV61avxnHaWIYZEeiYTT
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
